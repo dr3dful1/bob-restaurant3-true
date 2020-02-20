3d00cd258f1781b1342389496a73dc1c4f0e4d86
dr3dful1 <karmstr2@gmail.com>
2020-02-20 09:26:17 -0800

    pics added to main carousel

--- src/components/MainPageJumbotron.js
+++ src/components/MainPageJumbotron.js
@@ -17,47 +17,42 @@ function MainPageJumbotron(){
                 <div className="mainCarousel">
                     <Carousel showThumbs={false}>
                     <div>
-                        <img src="entrees.jpeg"  className="carousels" />
+                        <img src="jerkshrimp.jpg"  className="carousels" />
                         
                     </div>
                     <div>
-                        <img src="sides.jpeg"  className="carousels" />
+                        <img src="Chef_drops_onions.jpg"  className="carousels" />
                         
                     </div>
                     <div>
-                        <img src="entrees.jpeg"  className="carousels" />
+                        <img src="oxtails.jpg"  className="carousels" />
                         
                     </div>
                     <div>
-                        <img src="sides.jpeg"  className="carousels" />
+                        <img src="lobster_macaroni.jpg"  className="carousels" />
                         
                     </div>
                     <div>
-                        <img src="entrees.jpeg"  className="carousels" />
+                        <img src="coconut_chicken_curry.jpg"  className="carousels" />
                         
                     </div>
                     <div>
-                        <img src="sides.jpeg"  className="carousels" />
+                        <img src="beer_and_mpie.jpg"  className="carousels" />
                         
                     </div>
                     <div>
-                        <img src="entrees.jpeg"  className="carousels" />
+                        <img src="oxtail_roti.jpg"  className="carousels" />
                         
                     </div>
                     <div>
-                        <img src="sides.jpeg"  className="carousels" />
+                        <img src="shrimp_roti.jpg"  className="carousels" />
                         
                     </div>
                     <div>
-                        <img src="entrees.jpeg"  className="carousels" />
+                        <img src="chef_info.jpg"  className="carousels" />
                         
                     </div>
-                    <div>
-                        <img src="sides.jpeg"  className="carousels" />
-                        
-                    </div>
-                
-                
+                   
                 </Carousel>
                 </div>
                
