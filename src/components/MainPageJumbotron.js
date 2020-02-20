52a043ab5d13646c1f172e5db6278b321a7a3ab2
dr3dful1 <karmstr2@gmail.com>
2020-02-20 08:29:25 -0800

    Main page Carousel added
    carousel added . the pics have not been added and not sized correctly

--- src/components/MainPageJumbotron.js
+++ src/components/MainPageJumbotron.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer } from
 "mdbreact";
- 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 import CarouselPage from './CarouselPage'
 function MainPageJumbotron(){
 
@@ -10,6 +11,55 @@ function MainPageJumbotron(){
         <div className="container">
 
             <div className="jumbotron">
+                <div className="titleText">
+                    
+                </div>
+                <div className="mainCarousel">
+                    <Carousel showThumbs={false}>
+                    <div>
+                        <img src="entrees.jpeg"  className="carousels" />
+                        
+                    </div>
+                    <div>
+                        <img src="sides.jpeg"  className="carousels" />
+                        
+                    </div>
+                    <div>
+                        <img src="entrees.jpeg"  className="carousels" />
+                        
+                    </div>
+                    <div>
+                        <img src="sides.jpeg"  className="carousels" />
+                        
+                    </div>
+                    <div>
+                        <img src="entrees.jpeg"  className="carousels" />
+                        
+                    </div>
+                    <div>
+                        <img src="sides.jpeg"  className="carousels" />
+                        
+                    </div>
+                    <div>
+                        <img src="entrees.jpeg"  className="carousels" />
+                        
+                    </div>
+                    <div>
+                        <img src="sides.jpeg"  className="carousels" />
+                        
+                    </div>
+                    <div>
+                        <img src="entrees.jpeg"  className="carousels" />
+                        
+                    </div>
+                    <div>
+                        <img src="sides.jpeg"  className="carousels" />
+                        
+                    </div>
+                
+                
+                </Carousel>
+                </div>
                
             </div>
         </div>
